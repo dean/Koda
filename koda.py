3d898faac2ee0563c9c43f3aff94e3e5e4b227db
Dean Johnson <deanjohnson222@gmail.com>
2015-11-29 14:36:21 -0800

    Added "play something else" and "play something" triggers.

--- koda.py
+++ koda.py
@@ -12,15 +12,18 @@ import threading
 import speech_recognition as sr
 
 # Initialization
-r = sr.Recognizer()
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) {%(funcName)s} %(message)s',)
-triggers = ['coda', 'koda', 'cota', 'toyota', 'tota', 'kona']
+
 lock = {}
+song_queue = []
+triggers = ['coda', 'koda', 'cota', 'toyota', 'tota', 'kona']
+
 
 def capitalize(string):
     if not string:
         return ''
+
     f = string[0]
     minimum, maximum = ord('A'), ord('a')
     if minimum <= ord(f) < maximum:
@@ -36,13 +39,15 @@ def clean(string, chars=list('.()-_')):
     return functools.reduce(lambda original, ignore: original.replace(ignore, ''),
                   [string] + chars)
 
-# Stop programming music
-def stop_the_music():
+
+def _stop_the_music():
     global lock
+    global song_queue
     logging.debug("Killing mpg321")
+    song_queue = []
     try:
-        subprocess.check_call(['killall', 'mpg321'])
         lock['play music'] = False
+        subprocess.check_call(['killall', 'mpg321'])
     except subprocess.CalledProcessError:
         pass
     finally:
@@ -95,24 +100,37 @@ def _download(title, artist):
 
 
 def _play_music(title, artist):
+    filenames = get_filename(title=title, artist=artist)
+    queue_song(filename)
+    return
+
+
+def queue_song(filename):
+    global song_queue
+    if filename and not filename in song_queue:
+        song_queue.append(filename)
+    print('Song queue: {}'.format(song_queue))
+
+
+def play_song(filename):
     global lock
 
-    filename = get_filename(title=title, artist=artist)
-    if not filename or lock.get('play music'):
-        print('Locked or could not find file for: {0}'.format(filename))
-        return
-
+    # Play the music
     lock['play music'] = True
     logging.debug("Playing {path}".format(path=filename))
     try:
         subprocess.check_call(['mpg321', filename, '--quiet'])
+    except subprocess.CalledProcessError:
+        logging.debug('mpg321 killed.')
+
+    # Kill off mpg321
+    try:
         subprocess.check_call(['killall', 'mpg321'])
         logging.debug("Stopping mpg321")
     except subprocess.CalledProcessError:
-        logging.debug('mpg321 killed.')
-    finally:
-        lock['play music'] = False
-        return
+        pass
+    lock['play music'] = False
+    return
 
 
 def _play_something_music(artist=None):
@@ -120,37 +138,34 @@ def _play_something_music(artist=None):
 
     played_songs = []
     collisions = 0
+    filenames = get_filename(artist=artist)
+    if filenames:
+        for filename in filenames:
+            queue_song(filename)
+
+    return
+
+
+def _play_something_else():
+     try:
+        subprocess.check_call(['killall', 'mpg321'])
+        logging.debug("Stopping mpg321")
+     except subprocess.CalledProcessError:
+        pass
+     lock['play music'] = False
+     return
+
+
+def music_thread():
+    global song_queue
     while True:
-        filename = get_filename(artist=artist)
-        if collisions >= 5:
-            print('Exhausted all/most options for %s ' % artist)
-            return
+        while len(song_queue) > 0:
+            print(song_queue)
+            song = song_queue.pop()
+            print('Playing %s' % song)
+            play_song(song)
 
-        if filename in played_songs:
-            collisions += 1
-            continue
-
-        if not filename or lock.get('play music'):
-            return
-
-        played_songs.append(filename)
-        lock['play music'] = True
-        logging.debug("Playing {path}".format(path=filename))
-        try:
-            subprocess.check_call(['mpg321', filename, '--quiet'])
-        except subprocess.CalledProcessError:
-            logging.debug('mpg321 killed.')
-            return
-
-        try:
-            subprocess.check_call(['killall', 'mpg321'])
-            logging.debug("Stopping mpg321")
-        except subprocess.CalledProcessError:
-            pass
-        finally:
-            lock['play music'] = False
-        print('Continuing after playing "something".')
-
+        time.sleep(0.15)
     return
 
 
@@ -171,10 +186,6 @@ def listen_for_phrases():
 
             for i in matches:
                 spoken[i] = ''
-
-            print(matches)
-            print(spoken)
-            print('Triggered!')
 
             time_heard = datetime.datetime.now()
             lock['koda'] = True
@@ -212,38 +223,40 @@ def listen_for_phrases():
 
 
 def get_filename(title=None, artist=None):
-    print(artist)
-    artist = artist.lower().strip()
-    print(title, artist)
     init_path = '/Users/dean/Programming/YoutubeDownloaderClient/downloads/'
     songs = os.listdir(init_path)
-    songs = filter(lambda song: song.lower().startswith(artist), songs)
-    songs = list(songs)
+    if artist:
+        artist = artist.lower().strip()
+        songs = filter(lambda song: song.lower().startswith(artist), songs)
+        songs = list(songs)
 
-    if not title:
-        print('Choices: \n\t{0}'.format('\n\t'.join(songs)))
-        if not songs:
-            return ''
-        return os.path.join(init_path, random.choice(songs))
-    title = title.lower().strip()
-    matches = filter(lambda s: title in clean(s.lower()), songs)
-    x = list(matches)
-    print(x)
-    if len(x) > 0:
-        return os.path.join(init_path, sorted(x, key=len)[0])
-    print('Error... unable to parse from songs: {0}'.format(list(songs)))
-    return ''
+        if title:
+            matches = list(filter(lambda s: title in clean(s.lower()), songs))
+            if not matches:
+                print('Error... unable to parse from songs: {0}'.format(list(songs)))
+                return None
+            return os.path.join(init_path, sorted(matches, key=len)[0])
+
+        if songs:
+            print('Choices: \n\t{0}'.format('\n\t'.join(songs)))
+            return map(lambda x: os.path.join(init_path, x), songs)
+        return None
+
+    if songs:
+        return [os.path.join(init_path, random.choice(songs)) for _ in range(5)]
+
+    return None
 
 
 spoken = [''] * 10
 def await_commands():
     threading.Thread(name='Listen for phrases', target=listen_for_phrases).start()
+    threading.Thread(name='Music', target=music_thread).start()
     i = 1
     while(True):
         threading.Thread(name='Listener%d' % (i % 7), target=listen, args=(i % 7,)).start()
         time.sleep(2)
         i += 1
-
 
 
 # Speech to text
@@ -253,6 +266,7 @@ def listen(index):
     logging.debug("Listening...")
     with sr.Microphone() as source:  # use the default microphone as the audio source
         try:
+            r = sr.Recognizer()
             r.adjust_for_ambient_noise(source)
             audio = r.listen(source, MAX_WAIT)  # listen for the first phrase and extract it into audio data
             # print("Set minimum energy threshold to {}".format(r.energy_threshold))
@@ -267,20 +281,23 @@ def listen(index):
             return
     return
 
-MUSIC_SOMETHING_RE = r'play something'
-MUSIC_RE = r'play (.+) by (.+)'
+
+MUSIC_SOMETHING_ELSE_RE = r'play something else'
 MUSIC_SOMETHING_ARTIST_RE = r'play (?:something|a song) by (.+)'
+MUSIC_RE = r'play (.+) by (.+)'
+MUSIC_SOMETHING_RE = r'play something$'
 DOWNLOAD_RE = r'download (.+) by (.+)'
 STOP_MUSIC_RE = r'stop(?: the)? music'
 keyword_expressions = (
+    (MUSIC_SOMETHING_ELSE_RE, _play_something_else),
     (MUSIC_SOMETHING_ARTIST_RE, _play_something_music),
     (MUSIC_RE, _play_music),
-    (DOWNLOAD_RE, _download),
-    (STOP_MUSIC_RE, stop_the_music),
     (MUSIC_SOMETHING_RE, _play_something_music),
+    (DOWNLOAD_RE, _download),
+    (STOP_MUSIC_RE, _stop_the_music),
 )
+
 
 if __name__ == "__main__":
     await_commands()
-    logging.debug("END OF PROGRAM")
     logging.shutdown()
