f6a7324cfefbe3b413215a35252297c5730b023a
Matthew Hanson <tripledouble1210@gmail.com>
2016-03-18 19:13:58 -0700

    Use str.translate instead of reduce.

--- helpers.py
+++ helpers.py
@@ -1,10 +1,7 @@
-import functools
-
-
 def clean(string, chars=list('.()-_')):
     """Cleans a string of all characters we wish to ignore.
 
     @arg: string: String to clean
     """
-    return functools.reduce(lambda original, ignore: original.replace(ignore, ''),
-                  [string] + chars)
+    rosetta_stone = {ord(c): None for c in chars}
+    return string.translate(rosetta_stone)
